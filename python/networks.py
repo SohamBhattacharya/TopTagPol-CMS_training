b19f787c10d4c279ff0c3b93b6cdb834d74a98f7
SohamBhattacharya <soham.elessar@gmail.com>
2021-08-09 10:38:12 +0200

    Changes..

--- python/networks.py
+++ python/networks.py
@@ -13,10 +13,8 @@ d_network = {}
 def build_and_get_CNN1(input_shape, nCategory) :
     
     model = models.Sequential()
-    #inputLayer = layers.Input(shape = input_shape, sparse = False)
     model.add(layers.Lambda(function = utils.sparse_to_dense, input_shape = input_shape))
-    #model.add(layers.InputLayer(input_shape = img_shape, sparse = False))
-    model.add(layers.Conv2D(50, kernel_size = (10, 10), activation = "relu"))#, input_shape = img_shape))
+    model.add(layers.Conv2D(50, kernel_size = (10, 10), activation = "relu"))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(30, kernel_size = (5, 5), activation = "relu"))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -28,7 +26,30 @@ def build_and_get_CNN1(input_shape, nCategory) :
     model.add(layers.Dense(nCategory, activation = "relu"))
     model.add(layers.Dense(nCategory, activation = tensorflow.keras.activations.softmax))
     
-    
     return model
 
 d_network["CNN1"] = build_and_get_CNN1
+
+
+def build_and_get_CNN2(input_shape, nCategory) :
+    
+    model = models.Sequential()
+    model.add(layers.Lambda(function = utils.sparse_to_dense, input_shape = input_shape))
+    model.add(layers.Conv2D(50, kernel_size = (10, 10), activation = "relu"))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(30, kernel_size = (5, 5), activation = "relu"))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(10, kernel_size = (2, 2), activation = "relu"))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(rate = 0.2))
+    model.add(layers.Dense(200, activation = "relu"))
+    model.add(layers.Dropout(rate = 0.2))
+    model.add(layers.Dense(200, activation = "relu"))
+    model.add(layers.Dropout(rate = 0.2))
+    model.add(layers.Dense(200, activation = "relu"))
+    model.add(layers.Dense(nCategory, activation = "relu"))
+    model.add(layers.Dense(nCategory, activation = tensorflow.keras.activations.softmax))
+    
+    return model
+
+d_network["CNN2"] = build_and_get_CNN2
